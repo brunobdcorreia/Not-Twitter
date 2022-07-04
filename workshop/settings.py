5bf6c15829e7658bf48933c8cd77f10e63f07579
Bruno Bacelar <bruno.bacelar94@hotmail.com>
2022-07-04 20:46:14 -0300

    Update settings.py

--- workshop/settings.py
+++ workshop/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-tnt@o%$*57z7-h=@cbmg=b5sk$a0#j@2y0fcx)9zc=tu6s_h03'
+SECRET_KEY = 'redacted'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
